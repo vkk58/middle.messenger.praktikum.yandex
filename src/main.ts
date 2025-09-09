3a9408c26bfd886f6b9154bfdbf628f284445daf
vkk58 <twicvk@gmail.com>
2025-09-10 01:26:56 +0300

    qwetty

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
-import './styles/main.pcss';
-import './pages/profilePage/styles.pcss';
-import './pages/commonPage/styles.pcss';
-import './pages/errorPage/styles.pcss';
-import App from './APP';
-import { chatAPI } from './api/ChatApi';
+import "./styles/main.pcss";
+import "./pages/profilePage/styles.pcss";
+import "./pages/commonPage/styles.pcss";
+import "./pages/errorPage/styles.pcss";
+import App from "./APP";
+import { chatAPI } from "./api/ChatApi";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   const app = new App();
   app.start();
 });
-
-window.chatApi = chatAPI;
