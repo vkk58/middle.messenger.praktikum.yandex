591385dc30fae32202eb6de575881a318ec662dc
vkk58 <twicvk@gmail.com>
2025-09-11 00:00:51 +0300

    its ok

--- src/pages/commonPage/CommonPageController.ts
+++ src/pages/commonPage/CommonPageController.ts
@@ -1,7 +1,7 @@
-import { chatAPI } from '../../api/ChatApi';
-import PageRouter from '../../framework/PageRouter';
-import WebSocketController from '../../framework/WebSocketController';
-import CommonPage from './commonPage';
+import { chatAPI } from "../../api/ChatApi";
+import PageRouter from "../../framework/PageRouter";
+import WebSocketController from "../../framework/WebSocketController";
+import CommonPage from "./commonPage";
 
 export default class CommonPageController {
   private socketController: WebSocketController | null = null;
@@ -47,17 +47,17 @@ export default class CommonPageController {
 
   public async createChat() {
     let ret = false;
-    const el = document.getElementById('nameForNewChat') as HTMLInputElement;
-    if (el.value != '') {
+    const el = document.getElementById("nameForNewChat") as HTMLInputElement;
+    if (el.value != "") {
       try {
         const answer = await chatAPI.createChat(el.value);
-        const fileInput = document.getElementById('avatar') as HTMLInputElement;
+        const fileInput = document.getElementById("avatar") as HTMLInputElement;
 
         const avatarFile = fileInput.files?.[0];
         if (avatarFile) {
           const formData = new FormData();
-          formData.append('chatId', answer.id.toString());
-          formData.append('avatar', avatarFile);
+          formData.append("chatId", answer.id.toString());
+          formData.append("avatar", avatarFile);
 
           try {
             await chatAPI.uploadChatAvatar(formData);
@@ -70,16 +70,16 @@ export default class CommonPageController {
         alert(e);
       }
     } else {
-      alert('Название чата не может быть пустым');
+      alert("Название чата не может быть пустым");
     }
 
     return ret;
   }
 
   public async deleteChat() {
-    const chatElement = document.querySelector('.selectedCurrentChat');
+    const chatElement = document.querySelector(".selectedCurrentChat");
     if (chatElement) {
-      const id = Number(chatElement.getAttribute('id'));
+      const id = Number(chatElement.getAttribute("id"));
 
       if (id) {
         try {
@@ -92,29 +92,29 @@ export default class CommonPageController {
   }
 
   public static clearDialogBeforeCreate() {
-    const dialogContent = document.querySelector('.dialog-content');
+    const dialogContent = document.querySelector(".dialog-content");
     if (!dialogContent) return;
 
     const avatarInput = dialogContent.querySelector(
-      '#avatar',
+      "#avatar"
     ) as HTMLInputElement;
     if (avatarInput) {
-      avatarInput.value = '';
+      avatarInput.value = "";
     }
 
     const chatNameInput = dialogContent.querySelector(
-      '#nameForNewChat',
+      "#nameForNewChat"
     ) as HTMLInputElement;
     if (chatNameInput) {
-      chatNameInput.value = '';
+      chatNameInput.value = "";
     }
 
     const avatarImage = dialogContent.querySelector(
-      '.round-img',
+      ".round-img"
     ) as HTMLImageElement;
     if (avatarImage) {
       avatarImage.src =
-        'https://avatars.mds.yandex.net/get-yapic/58107/TKl7WKkXP1ybjbpKY7eyvAwGwi4-1/orig';
+        "https://avatars.mds.yandex.net/get-yapic/58107/TKl7WKkXP1ybjbpKY7eyvAwGwi4-1/orig";
     }
   }
 
@@ -138,13 +138,13 @@ export default class CommonPageController {
         this.socketController = new WebSocketController(
           this.userId,
           chatId,
-          token,
+          token
         );
         await this.socketController.connect();
         this.socketController.getOldMessages(0);
       }
     } catch (error) {
-      console.error('Ошибка инициализации WebSocket:', error);
+      console.error("Ошибка инициализации WebSocket:", error);
       throw error;
     }
   }
@@ -153,7 +153,7 @@ export default class CommonPageController {
     if (this.socketController && this.chatId) {
       this.socketController.sendMessage(message);
     } else {
-      console.error('WebSocket не инициализирован или чат не выбран');
+      console.error("WebSocket не инициализирован или чат не выбран");
     }
   }
 
@@ -172,13 +172,13 @@ export default class CommonPageController {
   }
 
   public async getUserSearchList() {
-    const elSearch = document.getElementById('search') as HTMLInputElement;
+    const elSearch = document.getElementById("search") as HTMLInputElement;
     if (elSearch && elSearch.value) {
       const answer = await chatAPI.getUserList(elSearch.value);
       const commonPage = PageRouter.getInstance().parmChangingPage();
       if (
         commonPage &&
-        typeof (commonPage as CommonPage).updateChats === 'function'
+        typeof (commonPage as CommonPage).updateChats === "function"
       ) {
         (commonPage as CommonPage).updateUserList(answer);
       }
